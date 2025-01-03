cb2835983f3fde2de2b9906e95b03791f2232b84
Zivorad Arsic <44918620+zilsKS@users.noreply.github.com>
2025-01-03 09:26:47 +0100

    Fix UI issues (#31)
    Co-authored-by: nokibuzz <nokibuzz311@gmail.com>

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ import AnimatedDog from "./AnimatedDog";
 const Navbar = ({ currentUser, translation }) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
-      <div className="py-4 border-b-[1px]">
+      <div className="py-2 border-b-[1px]">
         <Container>
           <div className="flex flex-row items-center justify-between gap-3 md:gap-0 h-full">
             <div className="flex-1">
