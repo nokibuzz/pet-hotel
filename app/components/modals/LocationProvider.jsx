38d02acdb708e74898b630a78298300b5584246e
Zivorad Arsic <arsiczivoradzile@gmail.com>
2025-04-24 23:23:20 +0200

    Fix one more issue for location

--- app/components/modals/LocationProvider.jsx
+++ app/components/modals/LocationProvider.jsx
@@ -7,21 +7,16 @@ import toast from "react-hot-toast";
 const LocationProvider = ({ currentUser }) => {
 
   const sendLocationToServer = async (latitude, longitude) => {
-    try {
-      if (!currentUser) {
-        return;
-      }
-      const userId = currentUser.id;
-      await axios
-        .post(`/api/location`, { userId, latitude, longitude })
-        .catch(() => {
-            toast.error("Woof, woof, something went wrong!")
-        })
-        .finally(() => {
-        });
-    } catch (error) {
-      console.error('Error saving user location:', error);
+    if (!currentUser) {
+      return;
     }
+    const userId = currentUser.id;
+    await axios
+      .post(`/api/location`, { userId, latitude, longitude })
+      .catch((error) => {
+          toast.error("Woof, woof, something went wrong!")
+          console.error('Error saving user location:', error);
+      });
   };
 
   useEffect(() => {
